ee7d5a2ea861de610d106ba7f405cb0ef0fa4724
willzglue <will@zglue.com>
2018-06-14 00:19:24 -0700

    added ionic flow. This is more popular than cordova

--- cordova_hello_world/hello/platforms/ios/platform_www/cordova_plugins.js
+++ cordova_hello_world/hello/platforms/ios/platform_www/cordova_plugins.js
@@ -31,13 +31,60 @@ module.exports = [
     "clobbers": [
       "navigator.geolocation"
     ]
+  },
+  {
+    "id": "cordova-plugin-buildinfo.BuildInfo",
+    "file": "plugins/cordova-plugin-buildinfo/www/buildinfo.js",
+    "pluginId": "cordova-plugin-buildinfo",
+    "clobbers": [
+      "BuildInfo"
+    ]
+  },
+  {
+    "id": "cordova-plugin-browsertab.BrowserTab",
+    "file": "plugins/cordova-plugin-browsertab/www/browsertab.js",
+    "pluginId": "cordova-plugin-browsertab",
+    "clobbers": [
+      "cordova.plugins.browsertab"
+    ]
+  },
+  {
+    "id": "cordova-plugin-inappbrowser.inappbrowser",
+    "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+    "pluginId": "cordova-plugin-inappbrowser",
+    "clobbers": [
+      "cordova.InAppBrowser.open",
+      "window.open"
+    ]
+  },
+  {
+    "id": "cordova-universal-links-plugin.universalLinks",
+    "file": "plugins/cordova-universal-links-plugin/www/universal_links.js",
+    "pluginId": "cordova-universal-links-plugin",
+    "clobbers": [
+      "universalLinks"
+    ]
+  },
+  {
+    "id": "cordova-plugin-firebase.FirebasePlugin",
+    "file": "plugins/cordova-plugin-firebase/www/firebase.js",
+    "pluginId": "cordova-plugin-firebase",
+    "clobbers": [
+      "FirebasePlugin"
+    ]
   }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
   "cordova-plugin-whitelist": "1.3.3",
-  "cordova-plugin-geolocation": "4.0.1"
+  "cordova-plugin-geolocation": "4.0.1",
+  "cordova-plugin-buildinfo": "2.0.1",
+  "cordova-plugin-compat": "1.2.0",
+  "cordova-plugin-browsertab": "0.2.0",
+  "cordova-plugin-inappbrowser": "3.0.0",
+  "cordova-universal-links-plugin": "1.2.1",
+  "cordova-plugin-firebase": "1.0.5"
 };
 // BOTTOM OF METADATA
 });
