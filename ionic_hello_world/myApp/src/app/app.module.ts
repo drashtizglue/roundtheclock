80dc2a64bfb556ce17bb97498356f00e3572d960
willzglue <will@zglue.com>
2018-06-17 18:57:37 -0700

    Attempted to use bluetooth, see attached tutorial
    This tutorial shows us how to run bluetooth. I can't test this on Ionic serve because it doesn't have bluetooth, will need to try on Emulator but currently can't do that
    https://ionicallyspeaking.com/2016/05/06/connecting-to-a-bluetooth-smart-sensor-with-ionic-2/

--- ionic_hello_world/myApp/src/app/app.module.ts
+++ ionic_hello_world/myApp/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
+import { BLE } from '@ionic-native/ble'; 
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
--- ionic_hello_world/myApp/src/pages/list/list.ts
+++ ionic_hello_world/myApp/src/pages/list/list.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
 import { Geolocation } from '@ionic-native/geolocation';
+import { BLE } from '@ionic-native/ble';
+
 
 @Component({
   selector: 'page-list',
@@ -9,26 +11,32 @@ import { Geolocation } from '@ionic-native/geolocation';
 export class ListPage {
   selectedItem: any;
   icons: string[];
+  devices: any;
   latitude: any;
   longitude: any;
   items: Array<{title: string, note: string, icon: string}>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public geolocation: Geolocation) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public geolocation: Geolocation, private ble: BLE) {
     // If we navigated to this page, we will have an item available as a nav param
     this.selectedItem = navParams.get('item');
 
     // Let's populate this page with some filler content for funzies
     this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
     'american-football', 'boat', 'bluetooth', 'build'];
+    this.devices = ['empty', 'nothing'];
 
     this.geolocation.getCurrentPosition().then((resp) => {
         this.latitude = resp.coords.latitude;
         this.longitude = resp.coords.longitude;
- // resp.coords.latitude
- // resp.coords.longitude
-}).catch((error) => {
-  console.log('Error getting location', error);
-});
+    // resp.coords.latitude
+    // resp.coords.longitude
+    }).catch((error) => {
+      console.log('Error getting location', error);
+    });
+
+    //this.ble.scan([], 10).subscribe(device => {
+    //  this.devices.push(device);
+    //});
     
     this.items = [];
     for (let i = 1; i < 11; i++) {
